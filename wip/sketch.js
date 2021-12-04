fcb0c3dac6a727a5f9b408780903e596494428ca
TeaBun <stephen.alberts@gmail.com>
2021-12-03 18:26:57 -0600

    42k block nebula

--- wip/sketch.js
+++ wip/sketch.js
@@ -5,7 +5,7 @@ function setup() {
   // document.body.style.marginTop = "10px";
 
   createCanvas(1880, 1040, WEBGL)
-  background(222)
+  background(0)
   
  
 
@@ -40,23 +40,82 @@ setInterval(function(){
   // var rgb = [r,g,b]
   // console.log(rgb)
 },100);
+// setInterval(function(){
+
+//   fill(0)
+//   rect(-(width/2),-(height/2),width,height)
+  
+// },100000);
 
 var r = 127
 var g = 127
 var b = 127
 
+var flipR
+var flipG
+var flipB
+
+var xstart = -940
+var ystart = -520
+var spacing = 90
+var xorigin = xstart
+var yorigin = ystart
+
+var x = 0
+var y = 0
+var movex = 0
+var movey = 0
+var grid = []
+
+var randomSize = 1
 
 function draw() {
-  fill(222)
-  noStroke()
+  movex = floor(random(-42,43))
+  movey = floor(random(-42,43))
+  x = x + movex
+  y = y + movey
+
+  var v = createVector(x, y)
+  grid.push(v)
+  
+  fill(r,g,b,100)
+  // noFill()
+  stroke(r,g,b,floor(random(10)))
+  // noStroke()
+  
   push()
-  translate(0,0,-100)
-  rect(-1000,-1000,2000, 2000)
+  translate(grid[grid.length-1].x,grid[grid.length-1].y)
+  rotateX(frameCount * 0.01);
+  rotateY(frameCount * 0.01);
+  randomSize = floor(randomSize + random(-(r + g + b)/9,(r + g + b)/9))
+  // console.log(randomSize)
+  box(r/randomSize,g/randomSize,b/randomSize)
   pop()
-  
-  
+  console.log(grid.length)
+  if(grid.length >= 42000){
+    noLoop()
+  }
 
 
+  if(randomSize > 300){
+    randomSize = randomSize - 20
+  }
+  if(randomSize < 50){
+    randomSize = randomSize + 10
+  }
+
+  if(x > ((width/2)-10)){
+    x = x - 10
+  }
+  if(y > ((height/2)-10)){
+    y = y - 10
+  }
+  if(x < -((width/2)-10)){
+    x = x + 10
+  }
+  if(y < -((height/2)-10)){
+    y = y + 10
+  }
 }
 
 var onoff = 1
